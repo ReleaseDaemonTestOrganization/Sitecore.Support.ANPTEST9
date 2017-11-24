6d9d667b05e4037b5f4c0eb5582e10bf78936e2d
Alen Pelin <alenpelin@gmail.com>
2017-11-24 14:44:40 +1100

    'test'

---
+++ src/Sitecore.Support.ANPTEST9/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.ANPTEST9")]
+[assembly: AssemblyProduct("Sitecore.Support.ANPTEST9")]
+[assembly: ComVisible(false)]
